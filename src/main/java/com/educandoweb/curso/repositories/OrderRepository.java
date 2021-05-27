31340a8afc6e9755671c047d5e6cda24a4c477f2
Paulo Soares <pbss2007@gmail.com>
2021-05-27 16:36:23 -0300

    Category

--- src/main/java/com/educandoweb/curso/repositories/OrderRepository.java
+++ src/main/java/com/educandoweb/curso/repositories/OrderRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.curso.entities.Order;
 
-
 public interface OrderRepository extends JpaRepository<Order, Long>  {
 
 }
