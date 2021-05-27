bf6697aabe22bbf2d358eca75d7360c049cb0b4e
Paulo Soares <pbss2007@gmail.com>
2021-05-27 14:03:02 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/curso/resources/UserResource.java
+++ src/main/java/com/educandoweb/curso/resources/UserResource.java
@@ -1,20 +1,39 @@
 package com.educandoweb.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.curso.entities.User;
+import com.educandoweb.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping()
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Paulo", "pbss@gmail.com", "998030152", "1234");
-		return ResponseEntity.ok(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		return ResponseEntity.ok(service.findById(id));
+	}
+
+//	@PostMapping
+//	@ResponseStatus(HttpStatus.CREATED)
+//	public User adicinarUser(@RequestBody User user) {
+//		return userRepository.save(user);
+//	}
 
 }
